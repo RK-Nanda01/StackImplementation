0c07da939a9f3d5f9eae2a8b1b203cf9566b0712
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-07-30 11:30:34 +0530

    [RK Nanda] Adding Queue.cs and Creating Queue

---
+++ MyStack/MyQueue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace MyStack
+{
+	public class MyQueue
+	{
+		private Node front;
+        private Node rear;
+        public MyQueue()
+		{
+			this.front = null;
+			this.rear = null;
+		}
+
+		public void Enqueue(int data)
+		{
+            Node node = new Node(data);
+            if (this.front == null)  // No Elements in Linked List
+            {
+                node.next = null;
+                front = node;
+                rear = node;
+
+            }
+            else
+            {
+                node.next = null;
+                rear.next = node;
+                rear = node;
+
+            }
+            
+            Console.WriteLine($"New Node Added {data}");
+        }
+        public void DisplayQueue()
+        {
+            Node temp = this.front;
+
+            if (temp == null)
+            {
+                Console.WriteLine($"Queue Empty");
+                return;
+            }
+
+            while (temp != null)
+            {
+                Console.Write($"{temp.data} ");
+                temp = temp.next;
+            }
+
+        }
+    }
+}
+
--- MyStack/Program.cs
+++ MyStack/Program.cs
@@ -3,18 +3,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Stack Implementation");
-        LinkedListStack myStack = new LinkedListStack();
-        myStack.Push(70);
-        myStack.Push(30);
-        myStack.Push(56);
-        Console.WriteLine("Before Popping");
-        myStack.DisplayStack();
-        Console.WriteLine("\n");
-        myStack.Pop();
-        myStack.Peek();
-        Console.WriteLine("After Popping");
-        myStack.DisplayStack();
+ 
+        MyQueue myq = new MyQueue();      
+        Console.WriteLine("Queue Implementation");
+        myq.Enqueue(56);
+        myq.Enqueue(30);
+        myq.Enqueue(70);
+        myq.DisplayQueue();
         Console.ReadLine();
     }
 }
